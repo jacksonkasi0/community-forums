d4d17932380a091ab90e1b28c99331c825021c72
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2025-03-08 23:46:15 +0530

    Add GitHub and Google icon components

---
+++ client/src/assets/icons/Google.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+
+export const GoogleIco: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='23'
+    height='25'
+    fill='none'
+    viewBox='0 0 23 25'
+    {...props}
+  >
+    <path
+      fill='#4285F4'
+      d='M17.29 18.951c2.022-1.886 2.917-5.017 2.38-8.021h-8.02v3.319h4.565a3.77 3.77 0 0 1-1.698 2.552z'
+    ></path>
+    <path
+      fill='#34A853'
+      d='M4.029 16.306A8.602 8.602 0 0 0 17.29 18.95l-2.774-2.15a5.133 5.133 0 0 1-7.664-2.689z'
+    ></path>
+    <path
+      fill='#FBBC02'
+      d='M6.853 14.112c-.358-1.118-.358-2.15 0-3.268l-2.824-2.2a8.66 8.66 0 0 0 0 7.67z'
+    ></path>
+    <path
+      fill='#EA4335'
+      d='M6.853 10.844c.99-3.097 5.198-4.889 8.023-2.243l2.466-2.416A8.46 8.46 0 0 0 4.029 8.644z'
+    ></path>
+  </svg>
+);
