b6d882131d584031e81a9f289790004f131fe2c3
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2025-03-08 20:46:56 +0530

    Add environment configuration for client and server with validation

---
+++ client/src/env/server.ts
@@ -0,0 +1,9 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    CLERK_SECRET_KEY: z.string().min(1),
+  },
+  experimental__runtimeEnv: process.env, // Safe it's only used in server side
+});
