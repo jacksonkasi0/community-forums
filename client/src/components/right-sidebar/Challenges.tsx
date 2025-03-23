dc994e72e1d01f277bc3cf8035cc28dc548a0bf9
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2025-03-23 17:27:10 +0530

    fix(challenges): update href in Challenge component for correct navigation

--- client/src/components/right-sidebar/Challenges.tsx
+++ client/src/components/right-sidebar/Challenges.tsx
@@ -31,7 +31,7 @@ const challenges: Challenge[] = [
     description: "Build a landing page or share your story!",
     image: "https://media2.dev.to/dynamic/image/width=880%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fi7uzu7m51rl9yhs2yryr.png",
     period: "MARCH 5 - APRIL 6",
-    href: "/challenges/wecoded",
+    href: "/wecoded",
     emoji: "💜"
   },
   {
